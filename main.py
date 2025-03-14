75679cf9799708a50eba0c9cd2407d6f39bb91ea
Sahasawat-Boss <Sahasawat.rk@gmail.com>
2025-03-14 17:33:20 +0700

    .

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
 print("Hello, Python!")
+print("Hello Github!")
+# Console Log in JS
--- note.py
+++ note.py
@@ -1,8 +1,18 @@
-#Data Types
+#Core Data Types
+
 #Int
+-267236
+76373
 
 #float
+272.0
+-9.7
 
 #String
+'hello'
+"hello"
+'4.12'
 
 #Bool
+True
+False
